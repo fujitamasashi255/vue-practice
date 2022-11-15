d25e7011042f595876e943395597d9398158eb8d
Masashi Fujita <masashifujita536@gmail.com>
2022-11-15 15:15:28 +0900

    6th subject 2nd_commit

--- app/javascript/pages/task/components/TaskCreateModal.vue
+++ app/javascript/pages/task/components/TaskCreateModal.vue
@@ -6,11 +6,11 @@
                     <div>
                         <div>
                             <label>タイトル</label>
-                            <input type="text" v-model="task.title"/>
+                            <input type="text" name="タイトル" v-model="task.title"/>
                         </div>
                         <div>
-                            <label>説明</label>
-                            <input type="text" v-model="task.description"/>
+                            <label>説明文</label>
+                            <input type="text" name="説明文" v-model="task.description"/>
                         </div>
                         <button type="button" @click.prevent="handleCreateTask">追加</button>
                         <button type="button" class="close" v-on:click="handleCloseModal">閉じる</button>
